aa6f084fd96bd6a32d139d539cefe81c3c922ef0
Nathan Nguyen <btnguyen@umich.edu>
2021-03-01 16:23:08 -0500

    Finished Author Routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
